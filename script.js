b9561836c4dcf51f835b5d772e47b65e9945c10f
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-04-28 13:45:57 -0400

    add for loop

---
+++ script.js
@@ -0,0 +1,12 @@
+//For Loops//
+//Instead of writing this//
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+//write something like this instead//
+for (var i = 4; i <= 23; i = i + 1) {
+	console.log(i);
+}
