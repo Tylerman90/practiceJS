1fbc45eb7db48244545a5cf46397349946308ebb
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-05-17 17:28:33 -0400

    switch statements

--- script.js
+++ script.js
@@ -141,3 +141,23 @@ var isEven = function(number) {
 for (i = 0; i < 10; i++) {
     console.log("all work no play makes tyler a dull boy");
 }
+
+//switch statements//
+var lunch = prompt("What do you want for lunch?","Type your lunch choice here");
+
+switch(lunch){
+  case 'sandwich':
+    console.log("Sure thing! One sandwich, coming up.");
+    break;
+  case 'soup':
+    console.log("Got it! Tomato's my favorite.");
+    break;
+  case 'salad':
+    console.log("Sounds good! How about a caesar salad?");
+    break;
+  case 'pie':
+    console.log("Pie's not a meal!");
+    break;
+  default:
+    console.log("Huh! I'm not sure what " + lunch + " is. How does a sandwich sound?");
+}
