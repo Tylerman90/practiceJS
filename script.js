7fb1681fa656422e4aa44a11098d9ee503928f51
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-05-17 17:31:54 -0400

    color prompt

--- script.js
+++ script.js
@@ -161,3 +161,20 @@ switch(lunch){
   default:
     console.log("Huh! I'm not sure what " + lunch + " is. How does a sandwich sound?");
 }
+
+//color prompter//
+var color = prompt("What's your favorite primary color?","Type your favorite color here");
+
+switch(color) {
+  case 'red':
+    console.log("Red's a good color!");
+    break;
+  case 'blue':
+    console.log("That's my favorite color, too!");
+    break;
+  case 'yellow':
+      console.log("That's the color of Big Bird!");
+      break;
+  default:
+    console.log("I don't think that's a primary color!");
+}
