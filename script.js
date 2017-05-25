6e74e41dcb270ac7c606ff399f9d7efd75d15eeb
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-05-25 18:05:02 -0400

    finish basic game

--- script.js
+++ script.js
@@ -268,3 +268,15 @@ switch(troll) {
       console.log("Dang! This troll only takes Troll Dollars. You get whomped!");
     }
     break;
+    case 'RUN':
+    var fast = prompt("Let's book it! Are you fast (YES or NO)?").toUpperCase();
+    var headStart = prompt("Did you get a head start?").toUpperCase();
+    if(fast === 'YES' || headStart === 'YES') {
+      console.log("You got away--barely! You live to stroll through the forest another day.");
+    } else {
+      console.log("You're not fast and you didn't get a head start? You never had a chance! The troll eats you.");
+    }
+    break;
+  default:
+    console.log("I didn't understand your choice. Hit Run and try again, this time picking FIGHT, PAY, or RUN!");
+}
